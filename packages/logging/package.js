c6ca480eaa67f414f8cc2079aa030c1b00b23de8
Avital Oliver <avital@thewe.net>
2013-05-14 15:06:00 -0700

    first pass towards a logging api

--- packages/logging/package.js
+++ packages/logging/package.js
@@ -5,11 +5,14 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
+  api.use(['underscore', 'ejson'], where);
+  api.add_files('debug.js', where);
   api.add_files('logging.js', where);
 });
 
 Package.on_test(function (api) {
   api.use('tinytest');
   api.use('logging', 'client');
+  api.add_files('debug_test.js', 'client');
   api.add_files('logging_test.js', 'client');
 });
